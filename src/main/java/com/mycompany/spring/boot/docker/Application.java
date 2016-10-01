205fe6923b266015b10d394b43c536de5fe1d1ce
Colin But <colinbut@users.noreply.github.com>
2016-10-01 19:46:37 +0100

    coded spring boot application with a simple endpoint

--- src/main/java/com/mycompany/spring/boot/docker/Application.java
+++ src/main/java/com/mycompany/spring/boot/docker/Application.java
@@ -1,11 +1,23 @@
 package com.mycompany.spring.boot.docker;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Spring Boot Application
  *
  * @author colin
  */
+@SpringBootApplication
+@RestController
 public class Application {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String home() {
+        return "Hello Docker World";
+    }
 
     public static void main(String[] args) {
         System.out.println("Spring Boot Application");
