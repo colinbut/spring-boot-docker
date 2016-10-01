df3e5ddd6dd9a0a1c3a68145d2b967a68cebd358
Colin But <colinbut@users.noreply.github.com>
2016-10-01 19:38:54 +0100

    cleaned up maven generated source code

---
+++ src/main/java/com/mycompany/spring/boot/docker/Application.java
@@ -0,0 +1,14 @@
+package com.mycompany.spring.boot.docker;
+
+/**
+ * Spring Boot Application
+ *
+ * @author colin
+ */
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Spring Boot Application");
+    }
+
+}
